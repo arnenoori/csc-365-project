c4daa723c70de05d28fe28d2f5f70a26ad334b5e
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2023-10-30 20:09:50 -0700

    fix server.py

--- src/api/server.py
+++ src/api/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, exceptions
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from src.api import transactions, admin, auth
+from src.api import transactions, admin, auth, users, purchases
 import json
 import logging
 import sys
@@ -22,34 +22,33 @@ app = FastAPI(
     },
 )
 
-# origins = ["https://potion-exchange.vercel.app"]
+origins = ["https://potion-exchange.vercel.app"]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["GET", "OPTIONS"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["*"],
+)
 
-# app.include_router(audit.router)
-# app.include_router(carts.router)
-# app.include_router(catalog.router)
-# app.include_router(bottler.router)
-# app.include_router(barrels.router)
-# app.include_router(admin.router)
+app.include_router(auth.router)
+app.include_router(transactions.router)
+app.include_router(users.router)
+app.include_router(purchases.router)
+app.include_router(admin.router)
 
-# @app.exception_handler(exceptions.RequestValidationError)
-# @app.exception_handler(ValidationError)
-# async def validation_exception_handler(request, exc):
-#     logging.error(f"The client sent invalid data!: {exc}")
-#     exc_json = json.loads(exc.json())
-#     response = {"message": [], "data": None}
-#     for error in exc_json:
-#         response['message'].append(f"{error['loc']}: {error['msg']}")
+@app.exception_handler(exceptions.RequestValidationError)
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request, exc):
+    logging.error(f"The client sent invalid data!: {exc}")
+    exc_json = json.loads(exc.json())
+    response = {"message": [], "data": None}
+    for error in exc_json:
+        response['message'].append(f"{error['loc']}: {error['msg']}")
 
-#     return JSONResponse(response, status_code=422)
+    return JSONResponse(response, status_code=422)
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello."}
+@app.get("/")
+async def root():
+    return {"message": "Welcome to our Receipt App Backend."}
