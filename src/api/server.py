76db7d3cbe5220b406881741799a017d4f1da1aa
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2023-10-30 20:40:58 -0700

    remove auth

--- src/api/server.py
+++ src/api/server.py
@@ -32,7 +32,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(auth.router)
 app.include_router(transactions.router)
 app.include_router(users.router)
 app.include_router(purchases.router)
