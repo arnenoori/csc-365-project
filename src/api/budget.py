d1270b334671e3e2b03da8bb67e57ad6c39b5a44
Bryan Viet Nguyen <bryancollege71@gmail.com>
2023-11-16 23:17:22 -0800

    Change tag from "budget" to "budgets" in get_all_purchases_categorized endpoint

--- src/api/budget.py
+++ src/api/budget.py
@@ -273,7 +273,7 @@ def compare_budgets_to_actual_spending(user_id: int, date_from: str = None, date
     return comparisons
 
 # gets sum of money spent of different catagories of all purchases for a user
-@router.get("/categories", tags=["budget"])
+@router.get("/categories", tags=["budgets"])
 def get_all_purchases_categorized(user_id: int):
     """ """
     ans = []
