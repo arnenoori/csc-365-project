297b8d893c325f67fca5e5e2010da41c8b50f3fc
Arne <arnenoori@gmail.com>
2023-12-02 20:21:44 -0800

    more budget tests

--- src/tests/budget_tests.py
+++ src/tests/budget_tests.py
@@ -373,27 +373,27 @@ class TestIsValidDate:
 
         # Returns False when input is False
     def test_returns_false_when_input_is_false(self):
-        assert not code_under_test(False)
+        assert not is_valid_date(False)
 
     # Returns False when input is None
     def test_returns_false_when_input_is_none(self):
-        assert not code_under_test(None)
+        assert not is_valid_date(None)
 
     # Returns False when input is an empty string
     def test_returns_false_when_input_is_empty_string(self):
-        assert not code_under_test('')
+        assert not is_valid_date('')
 
     # Returns True when input is a non-empty string
     def test_returns_true_when_input_is_non_empty_string(self):
-        assert code_under_test('hello')
+        assert is_valid_date('hello')
 
     # Returns True when input is a non-empty list
     def test_returns_true_when_input_is_non_empty_list(self):
-        assert code_under_test([1, 2, 3])
+        assert is_valid_date([1, 2, 3])
 
     # Returns True when input is a non-empty dictionary
     def test_returns_true_when_input_is_non_empty_dictionary(self):
-        assert code_under_test({'key': 'value'})
+        assert is_valid_date({'key': 'value'})
 
 
 class TestIsFirstDayOfMonth:
